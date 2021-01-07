3a0658b595195a5cc6a22ddceb40ee71fb9b9a6b
chayawacsstock <chaya.wacsstock@brandcust.co.il>
2021-01-07 12:19:09 +0200

    upload pannel

--- upload-angular/src/app/uploader/uploader.component.ts
+++ upload-angular/src/app/uploader/uploader.component.ts
@@ -8,16 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class UploaderComponent implements OnInit {
 
   files: File[] = [];
-  
+  panelOpenState = false;
+  isClose: boolean = true;
+  expandLess: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   uploadImage(event: any) {
+    this.isClose = false;
     for (let i = 0; i < event.files.length; i++) {
       this.files.push(event.files.item(i));
     }
+  }
+
+  close() {
+    this.isClose = true;
+    this.files = [];
   }
 
 }
