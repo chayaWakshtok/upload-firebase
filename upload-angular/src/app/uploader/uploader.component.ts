afa18829362c285811383d71c816b26da01d30e8
chayawacsstock <chaya.wacsstock@brandcust.co.il>
2021-01-07 12:06:09 +0200

    upload file input

--- upload-angular/src/app/uploader/uploader.component.ts
+++ upload-angular/src/app/uploader/uploader.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploaderComponent implements OnInit {
 
+  files: File[] = [];
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  uploadImage(event: any) {
+    for (let i = 0; i < event.files.length; i++) {
+      this.files.push(event.files.item(i));
+    }
   }
 
 }
